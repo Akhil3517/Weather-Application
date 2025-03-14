d4caffc2648e60fd43864598a4204853f575879b
Akhil3517 <akhil35177@gmail.com>
2025-03-14 13:13:30 +0530

    changes

--- script.js
+++ script.js
@@ -1,55 +1,61 @@
 const API_KEY = "97453c013f265647d9de207ae7525aeb";
-const Api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
-
-const searchbox =document.querySelector(".search input");
-const searchbtn =document.querySelector(".search button");
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
 const weather = document.querySelector(".weather");
 
+const getWeather = async (city) => {
+    weather.innerHTML = `<h2>Loading...</h2>`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-const getWeather = async(city) => {
-    weather.innerHTML = `<h2> Loading... <h2>`
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-    const response = await fetch(url);
-    const data = await response.json()
-    return showWeather(data)
-}
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        showWeather(data);
+    } catch (error) {
+        weather.innerHTML = `<h3>Error fetching data</h3>`;
+    }
+};
 
 const showWeather = (data) => {
-    if (data.cod == "404") {
-        weather.innerHTML = `<h3> City not found</h3>`
+    if (data.cod === "404") {
+        weather.innerHTML = `<h3>City not found</h3>`;
         return;
     }
+
     weather.innerHTML = `
         <div>
-            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="">
+            <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png" alt="Weather Icon">
         </div>
         <div>
-            
             <h2>${Math.round(data.main.temp)} ℃</h2>
-            <h4> ${data.weather[0].main} </h4>
-            <div>
-            <h3 class="city">${data.name}</h3> <div>
+            <h4>${data.weather[0].main}</h4>
+            <h3 class="city">${data.name}</h3>
         </div>
-            <div class="details">
-                <div class="col">
-                    <img src="i1.png" >
-                    <div > 
-                        <p class="humidity">${data.main.humidity} %</p>
-                        <p>humid</p>
-                    </div>
+        <div class="details">
+            <div class="col">
+                <img src="assets/humidity.png" alt="Humidity">
+                <div>
+                    <p class="humidity">${data.main.humidity}%</p>
+                    <p>Humidity</p>
                 </div>
-                <div class="col">
-                    <img src = "image.png">
-                    <div >
-                        <p class="wind">${data.wind.speed}km/h</p>
-                        <p >wind speed</p>
-    `
-}
-searchbtn.addEventListener("click",()=>{
-    getWeather(searchbox.value);
-})
-searchbox.addEventListener("keydown", (event) => {
+            </div>
+            <div class="col">
+                <img src="assets/image.png" alt="Wind Speed">
+                <div>
+                    <p class="wind">${data.wind.speed} km/h</p>
+                    <p>Wind Speed</p>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+searchBtn.addEventListener("click", () => {
+    getWeather(searchBox.value);
+});
+
+searchBox.addEventListener("keydown", (event) => {
     if (event.key === "Enter") {
-        getWeather(searchbox.value);
+        getWeather(searchBox.value);
     }
 });
